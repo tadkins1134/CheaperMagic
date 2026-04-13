bc27f95198265463206862794cfb9fc764eb7c78
tadkins1134 <tadkins1134@gmail.com>
2026-04-12 21:56:29 -0400

    Added basic functionality so the user can interact with the bot and add or remove items from the card list and retreive the current list they have

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CheaperMagic
 This is a discord bot that will poll for magic cards of interest and update the user that the card is around a decent price
+
+TODO:
+1. Setup the bot to allow users to track their own list of cards with a given desired price
+    -This list currently will be stored in memory and lost if the bot restarts. 
+    -Future stretch goal would be to add a database to hold this information the persist across restarts.
+2. Obtain access to an API from TCGPlayer to gain access to the cards
+3. Add polling mechanism to retrieve new card data at the users desired time frame
+
+
+Learned Lessons:
+1. To keep libraries separate and not cause issues on system installs I am using a venv which is just a virtual enviornment so I can use the libraries needed but contain them here.
---
+++ usersCards.py
@@ -0,0 +1,69 @@
+import discord
+from discord.ext import commands
+from dataclasses import dataclass
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+magicBot = commands.Bot(command_prefix='$', intents=intents)
+
+user = {}
+
+@dataclass
+class Card:
+    name: str
+    price: float
+
+#Main function to call and build out the users card list with all necessary information.
+async def main():
+   cardList = addCardsList()
+
+@magicBot.event
+async def on_ready():
+    print(f'We have logged in as {magicBot.user}')
+
+#This method is to allow the user to add cards to a list that will be scraped for on TCGPlayer
+@magicBot.command(name='cards')
+async def addCardsList(ctx,  price, *, card):
+    if ctx.author not in user:
+        user[ctx.author] = []
+    usersCardList = user[ctx.author]
+    usersCardList.append(Card(price=price, name=card))    
+    await ctx.send(f'Added {card} with desired price of ${price}. Total cards in list now: {len(usersCardList)}')
+
+#This method is so the user can see the card they have added to their list
+@magicBot.command(name='myCards')
+async def myCards(ctx):
+    if ctx.author not in user:
+        await ctx.send("You don't have any cards in your list.")
+        return
+    usersCardList = user[ctx.author]
+    await ctx.send(f'Your cards: {", ".join([card.name for card in usersCardList])}')
+
+#This method is used to clear a single card or all cards from the users card list
+@magicBot.command(name='clearCards')
+async def clearCards(ctx, card):
+    if card == "all":
+        if ctx.author in user:
+            user[ctx.author] = []
+            await ctx.send("Cleared all cards from your list.")
+        else:
+            await ctx.send("You don't have any cards in your list.")
+    elif ctx.author in user and card in [c.name for c in user[ctx.author]]:
+        usersCardList = user[ctx.author]
+        usersCardList = [c for c in usersCardList if c.name != card]
+        user[ctx.author] = usersCardList
+        await ctx.send(f'Removed {card} from your list. Total cards in list now: {len(usersCardList)}')
+    elif ctx.author in user:
+        await ctx.send(f'{card} is not in your list. Total cards in list: {len(user[ctx.author])}')
+    else:
+        await ctx.send("You don't have any cards in your list.")
+
+
+#This method is used by the user to scrape TCGPlayer for the cards in their list and return any that are at or below the price they set.
+@magicBot.command(name='getMyCards')
+async def cardSiteScraper():
+
+
+magicBot.run('MTQ4ODAwMjQ4ODIyNDE4NjQ3MA.GIXaqi.wTjMIfb2QTGwLYklhSV1eo84MsyXuMrETRRxXM')
